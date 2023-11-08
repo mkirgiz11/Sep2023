d05eff45abd96c1da5168a2e755801dbff9963c1
mkirgiz11 <mkirgiz11@gmail.com>
2023-11-08 01:10:06 +0000

    adding automated committing program

---
+++ git_push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git add .
+if [ $# -le 0 ]; then
+    read -p "Please enter commit message: " commit_message
+    git commit -m "$commit_message"
+else
+    git commit -m $1
+fi
+
+git push 
+
+
