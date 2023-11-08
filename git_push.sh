cd1b57b8d1fa020fbbce3eec5e6b8342cb110df6
mkirgiz11 <mkirgiz11@gmail.com>
2023-11-08 02:11:01 +0000

    Automated Committing program V.1

--- git_push.sh
+++ git_push.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-git add .
-if [ $# -le 0 ]; then
-    read -p "Please enter commit message: " commit_message
-    git commit -m "$commit_message"
+if [ -d ".git" ]; then
+    git add .
+    if [ $# -le 0 ]; then
+        read -p "Please enter commit message: " commit_message
+        git commit -m "$commit_message"
+    else
+        git commit -m "$1"
+    fi
+
+    git push 
 else
-    git commit -m "$1"
+    echo "You are not in a repository "
 fi
-
-git push 
-
 
