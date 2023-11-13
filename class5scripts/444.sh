75a628419e4e635a502afb6c5ca4a0ff3263fe61
mkirgiz11 <mkirgiz11@gmail.com>
2023-11-13 00:50:30 +0000

    Changes to the homework1

--- class5scripts/444.sh
+++ class5scripts/444.sh
@@ -2,16 +2,13 @@
 
 droplets=$(curl -s -H "Authorization: Bearer $DO_TOKEN" \
 "https://api.digitalocean.com/v2/droplets" | jq -r '.droplets[].id')
-#API CALL to Digital ocean
 
-#for loop To get droplet id's
 for DROPLET_ID in $droplets; do
     response=$(curl -s -X GET \
       -H "Content-Type: application/json" \
       -H "Authorization: Bearer $DO_TOKEN" \
       "https://api.digitalocean.com/v2/droplets/$DROPLET_ID")
 
-    ##To see if there are errors in API connection.
     error=$(echo "$response" | jq -r '.message')
     if [ "$error" != "null" ]; then
         echo "Error for Droplet $DROPLET_ID: $error, Please check connection"
@@ -19,7 +16,7 @@ for DROPLET_ID in $droplets; do
         exit 1
     else
         droplet_status=$(echo "$response" | jq -r '.droplet.status')
-        ## If condition to see if it is active, to print IP address, if it cant for any reason, error message.
+
         if [ "$droplet_status" == "active" ]; then
             for droplet in $(echo "$response" | jq -c '.droplet.networks.v4[]'); do
                 ipv4=$(echo "$droplet" | jq -r '.ip_address')
@@ -31,13 +28,6 @@ for DROPLET_ID in $droplets; do
             done
         else
             echo "Droplet $DROPLET_ID is inactive."
-            ## Else print out that droplet is inactive.
         fi
     fi
 done
-
-
-
-
-
- 
